0e60670ed260fd99fb7a5498775c202f465bbeb4
illiant <161250129@smail.nju.edu.cn>
2018-06-16 14:27:04 +0800

    status_card组件

---
+++ web/src/components/common/status_card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="lee-status-card">
+    <div class="value">
+      <div class="count">{{ count }}</div>
+      <span>{{ name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'statusCard',
+    props: ['name', 'count']
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .lee-status-card {
+  display: flex;
+  background-color: white;
+  border-radius: .4rem;
+  min-height: 10rem;
+  overflow: hidden;
+
+  .symbol {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+    color: white;
+    font-size: 4.5rem;
+  }
+  .value {
+    color: gray;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-grow: 1;
+
+    .count {
+      font-size: 2.4rem;
+    }
+    span {
+      font-size: 1.2rem;
+    }
+  }
+}
+
+</style>
+
+
